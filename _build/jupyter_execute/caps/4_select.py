7d715a253f33ce77ad745981d3fcf0847dcc7272
Alberto Benavides <bena.87@hotmail.com>
2022-09-01 13:04:37 -0500

    Agregado ejemplo tarea cap 2

---
+++ _build/jupyter_execute/caps/4_select.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Consultas
+
+# ## SELECT
+# 
+# ## Operaciones matemáticas
+# 
+# ## WHERE
+# 
+# ## LIKE ILIKE
+# 
+# ## DISTINCT
+# 
+# ## ORDER BY
+# 
+# ## AND OR
+# 
+# ## CAST CAST (shortcut)
+
+# ## Tarea
+# 
+# ### Instrucciones
+# 1. 
+# 
+# Puntos extra
+# - 
+# 
+# ### Ejemplo
+# 
